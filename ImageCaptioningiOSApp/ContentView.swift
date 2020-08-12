09c7bcbbeedec5717f39b39ae573ab2a83474b6a
Thomas Lam <tuananhlam95@gmail.com>
2020-08-11 23:49:47 -0400

    basic image picker done

--- ImageCaptioningiOSApp/ContentView.swift
+++ ImageCaptioningiOSApp/ContentView.swift
@@ -9,8 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                    
+                    Text("Photo library")
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .padding(.horizontal)
+            }
+        }
+        .sheet(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
     }
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+#  ROADMAP
+- [ ] Allow camera
+- [ ] Photo Library
+- [ ] Pytorch CNN + LSTM for image captioning
+- [ ] Convert Pytorch model into CoreML
+- [ ] Integrate CoreML into app
+
