257097a1fa31c3adf616a961a455f551d2b1c30e
Thomas Lam <tuananhlam95@gmail.com>
2020-08-11 20:47:21 -0400

    Initial Commit

---
+++ ImageCaptioningiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageCaptioningiOSApp
+//
+//  Created by Thomas Lam on 8/11/20.
+//  Copyright © 2020 Thomas Lam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
